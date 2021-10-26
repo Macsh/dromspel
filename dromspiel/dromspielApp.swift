4c055fd2da4a64dd448b05a5ef30b612931432a3
dreamteam <a.menskoi@gmail.com>
2021-10-26 09:51:08 +0200

    First

---
+++ dromspiel/dromspielApp.swift
@@ -0,0 +1,17 @@
+//
+//  dromspielApp.swift
+//  dromspiel
+//
+//  Created by alexandre on 26/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct dromspielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
