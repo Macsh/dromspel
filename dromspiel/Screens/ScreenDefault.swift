f5bd41ee55d9c969282f22defe416f3220397d2a
dreamteam <a.menskoi@gmail.com>
2021-10-26 10:00:58 +0200

    Note

---
+++ dromspiel/Screens/ScreenDefault.swift
@@ -0,0 +1,20 @@
+//
+//  ScreenDefault.swift
+//  dromspiel
+//
+//  Created by alexandre on 26/10/2021.
+//
+
+import SwiftUI
+
+struct ScreenDefault: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScreenDefault_Previews: PreviewProvider {
+    static var previews: some View {
+        ScreenDefault()
+    }
+}
