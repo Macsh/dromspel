452c36e78790da57a87ed9d5de38643b7c0831fe
Alexandre Menskoï <a.menskoi@gmail.com>
2021-10-27 10:09:41 +0200

    Change name

---
+++ dromspel/dromspelApp.swift
@@ -0,0 +1,17 @@
+//
+//  dromspielApp.swift
+//  dromspiel
+//
+//  Created by alexandre on 26/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct dromspelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
